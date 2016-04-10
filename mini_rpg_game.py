e19f2bd061a6fd26341aa1973d3df9edab522704
dondonsp <melky.duenas@gmail.com>
2016-04-10 15:54:38 -0300

    mini

--- mini_rpg_game.py
+++ mini_rpg_game.py
@@ -11,6 +11,10 @@ class Attributes:
 		self.charisma = cha
 		self.perception = per
 		self.luck = luck
+		self.health = (stre + res + sta) / 3
+		self.max_health = (stre + res + sta) / 3
+		self.magic = (dex + inte + per) / 3
+		self.max_magic = (dex + inte + per) / 3
 	
 
 ## Class for char
@@ -21,7 +25,11 @@ class Char:
 		self.race = race
 		self.classe = classe
 		self.attributes = attr
+		self.level = 1
 	
+	def get_level(self):
+		return self.level
+
 	def get_genre(self):
 		return self.genre
 	
@@ -34,19 +42,70 @@ class Char:
 	def get_classe(self):
 		return self.classe
 	
-	def get_attributes(self, at):
-		return self.attributes.
+	def get_attributes(self, source):
+		dataSource = getattr(self.attributes, source)
+		return dataSource
+
+	def set_attributes(self, source, val):
+		dataSource = getattr(self.attributes, source)
+		dataSource = val
+
+	def set_health(self, flag, val):
+		if flag == 'pos':
+			self.attributes.health += val
+		elif flag == 'neg':
+			self.attributes.health -= val
+		else:
+			return
+
+	def set_magic(self, flag, val):
+		if flag  == 'pos':
+			self.attributes.magic += val
+		elif flag == 'neg':
+			self.attributes.magic -= val
+		else:
+			return 
 
 ## Tests for classes
 attr1 = Attributes(10, 10 ,10, 10, 5, 10, 10 ,10)
-char1 = Char('M', 'Adan', 'human', 'warrior', attr1)
+char1 = Char('M', 'Adan', 'ork', 'warrior', attr1)
 
-print char1.get_name()
-print char1.get_genre()
-print char1.get_race()
-print char1.get_classe()
-print char1.get_attributes(inteligence)
-print dir(Char)
+attr2 = Attributes(8, 8, 8, 8, 4, 8, 8, 8)
+char2 = Char('F', 'Aria', 'elf', 'mage', attr2)
+
+## Function to show char's profile
+def show_char(char):
+	print char.get_name()
+	print 'Level: ' + str(char.get_level())
+	if char.get_genre() == 'F':
+		print 'Genre: Famale'
+	else:
+		print 'Genre: Male'
+	print 'Race: ' +  char.get_race()
+	print 'Class: ' + char.get_classe()
+
+	print 20 * '-'
+	## Show attributes
+	print 'Attributes'
+	print 'HP: ' + str(char.get_attributes('health')) + '/' + str(char.get_attributes('max_health'))
+	print 'MP: ' + str(char.get_attributes('magic')) + '/' + str(char.get_attributes('max_magic'))
+	print 'Strength: ' + str(char.get_attributes('strength'))
+	print 'Resistance: ' + str(char.get_attributes('resistance'))
+	print 'Dexterity: '  + str(char.get_attributes('dexterity'))
+	print 'Stamina: ' + str(char.get_attributes('stamina'))
+	print 'Inteligence: ' + str(char.get_attributes('inteligence'))
+	print 'Charisma: ' + str(char.get_attributes('charisma'))
+	print 'Perception: ' + str(char.get_attributes('perception'))
+	print 'Luck: ' + str(char.get_attributes('luck'))
+
+	print 20 * '#'
+
+char1.set_health('neg', 6)
+char1.set_magic('sss', 4)
+char2.set_magic('neg', 3)
+char2.set_attributes('luck', 5)
+show_char(char1)
+show_char(char2)
 
 ## Testing git
 ## Testing git again
